fb4df2aa8634af43b73557009cc8202081f8a5d3
GREENWHITE123 <144606839+GREENWHITE123@users.noreply.github.com>
2023-10-01 21:25:12 +0700

    Create main.py
    mc;lsadsadmsdsad;ad'dsaddkad;adakd;ladlakdakd;ala;lkda;lkdksakdadka;da;ldksakd;lakd;lalk;ldsadsa;ldlad;lad;ladka;ldk;lakd;lakd;lakd;ladad;ald

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=discord.Intents.default())
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+bot.run("MTE1NTQ5OTczMjI5NTIyNTQzNA.Gywfuk.5aMOLrDKiBEAlhf2C3kpdpqoe40Nnow9SzFTAE")
